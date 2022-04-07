16b740f5381fba2f205517ec0b936e737f04507d
Diego Duarte <duartqx@gmail.com>
2022-04-06 22:10:32 -0300

    added notify-send pipe function

--- send2notify.sh
+++ send2notify.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
-URL_regex='^(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]\.[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]$'
+URL_regex='^(https?|ftp|file)'
+XCPS='string:x-canonical-private-synchronous:SingleNotification'
+pipedNotify() {
+    while read PipedNotification; do
+        notify-send -h "$XCPS" "$PipedNotification";
+    done
+}
 
-[[ ! $1 =~ $URL_regex ]] && exit 1
+case $1 in
+    -p)
+        pipedNotify ;;
+    *)
+        [[ ! $1 =~ $URL_regex ]] && exit 1
 
-send2playlist $1
-
-if [[ "$?" == "0" ]]; then
-    notify-send -u low 'Sent to Playlist'
-else
-    notify-send -u low 'Something went wrong'
-fi
+        send2playlist $1
+        
+        if [[ "$?" == "0" ]]; then
+            notify-send -u low 'Sent to Playlist'
+        else
+            notify-send -u low 'Something went wrong'
+        fi
+esac
