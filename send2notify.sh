5443bb442eab321c5086179a7794677eb7a12cae
Diego Duarte <duartqx@gmail.com>
2022-04-07 20:12:26 -0300

    added x-canonical to sending to playlist notification

--- send2notify.sh
+++ send2notify.sh
@@ -14,13 +14,13 @@ case $1 in
     *)
         [[ ! $1 =~ $URL_regex ]] && exit 1
 
-        notify-send 'Sending to Playlist'
+        notify-send -h "$XCPS" 'Sending to Playlist'
 
         send2playlist $1
         
         if [[ "$?" == "0" ]]; then
-            notify-send -u low 'Sent to Playlist'
+            notify-send -h "$XCPS" -u low 'Sent to Playlist'
         else
-            notify-send -u low 'Something went wrong'
+            notify-send -h "$XCPS" -u low 'Something went wrong'
         fi
 esac
