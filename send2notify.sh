a8818cdb38dafc8c01c230b3329612a34852cc04
Diego Duarte <duartqx@gmail.com>
2022-04-13 13:31:55 -0300

    added comment about SingleNotification

--- send2notify.sh
+++ send2notify.sh
@@ -2,6 +2,13 @@
 
 URL_regex='^(https?|ftp|file)'
 XCPS='string:x-canonical-private-synchronous:SingleNotification'
+# $XCPS is a notification option that keeps a single notification rather than a
+# flood of many 
+#(the SingleNotification part of it is a variable that doesn't # matter what
+#it's called).
+# It works by setting the SingleNotification as a title for all the
+# notifications that "while read" get piped into it and allowing only a single
+# notification window of the same name on screen.
 pipedNotify() {
     while read PipedNotification; do
         notify-send -h "$XCPS" "$PipedNotification";
@@ -12,7 +19,9 @@ case $1 in
     -p)
         pipedNotify ;;
     -s)
-        # If $1 == -s && $2 exists and is an URL Sends it to send2playlist
+        # If $1 == -s && $2 ins't an empty string and is an URL by checking via
+        # the regex operator =~ if all these tests pass $2 is sent to
+        # send2playlist
         [[ -z $2 || ! $2 =~ $URL_regex ]] && exit 1
 
         notify-send -h "$XCPS" 'Sending to Playlist'
