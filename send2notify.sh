b85fa2755206606b7f937014d63a99496b69be38
Diego Duarte <duartqx@gmail.com>
2022-04-13 13:36:04 -0300

    added comment about SingleNotification

--- send2notify.sh
+++ send2notify.sh
@@ -2,13 +2,14 @@
 
 URL_regex='^(https?|ftp|file)'
 XCPS='string:x-canonical-private-synchronous:SingleNotification'
-# $XCPS is a notification option that keeps a single notification rather than a
-# flood of many 
-#(the SingleNotification part of it is a variable that doesn't # matter what
-#it's called).
-# It works by setting the SingleNotification as a title for all the
+# $XCPS is a notification argument for the -h option of notify-send that
+# ensures a single notification window is on screen at any time, rather than a
+# flood of many during youtube-dl download
+# (the SingleNotification part of it is a variable that doesn't matter what
+# it's called).
+# It works by setting SingleNotification as a title for all the
 # notifications that "while read" get piped into it and allowing only a single
-# notification window of the same name on screen.
+# window of the same name on screen.
 pipedNotify() {
     while read PipedNotification; do
         notify-send -h "$XCPS" "$PipedNotification";
